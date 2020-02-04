c8366744ceccbe278f7358585d9ee559647a4472
Nils Asejevs <nils.asejevs@gmail.com>
2020-02-04 09:48:00 +0200

    First commit

---
+++ src/scala/Texture.scala
@@ -0,0 +1,12 @@
+package coolGame;
+
+class Texture {
+    println("constructor called!");
+    def loadTexture(path: String) = {
+        println("hello world!");
+    }
+
+    def saveTexture(path: String) = {
+        println("hello world!");
+    }
+}
